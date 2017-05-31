06efbd56b2f76e68310585cc5105d7735b0b7725
Sravan742 <sivasravan007@gmail.com>
2017-05-31 08:25:12 +0530

    Add files via upload

---
+++ w3schools JS/js/toggleJavaScript.js
@@ -0,0 +1,8 @@
+function togglefunction(){
+	if(document.getElementById('demo').innerHTML = 'Turn on'){
+		document.getElementById('demo').innerHTML = 'Hello JavaScript'
+	}
+	else if (document.getElementById('demo').innerHTML = 'Hello JavaScript'){
+		document.getElementById('demo').innerHTML = 'Turn on'	
+	}
+}
